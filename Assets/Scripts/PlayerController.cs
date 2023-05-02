528272be8684f73b80511678d720261e83edf099
DylanMcPherson1 <124711542+DylanMcPherson1@users.noreply.github.com>
2023-05-02 11:57:52 -0400

    started game over screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,20 +8,21 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D player;
     int score = 0;
     public Text scoreUI;
+    public bool isAlive;
 
     void Start()
     {
 
         player = GetComponent<Rigidbody2D>();
-        
+        isAlive = true;
     }
 
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isAlive)
         {
-            player.AddForce(new Vector2(0,1) * 300);
+            player.AddForce(new Vector2(0,1) * 400);
         }
     }
     void OnTriggerEnter2D (Collider2D other)
@@ -31,6 +32,11 @@ public class PlayerController : MonoBehaviour
             score = score + 1;
             scoreUI.text = score.ToString();
         }
+        
+    }
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        isAlive = false;
     }
  
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,21 +7,28 @@ public class SpawnManager : MonoBehaviour
     public GameObject pipesPrefab;
 
     float randomHeight = 0.5f;
+    PlayerController playerScript;
 
     void Start()
     {
-        InvokeRepeating("SpawnPipes", 0.5f, 2.0f);
+        InvokeRepeating("SpawnPipes", 0.5f, 2f);
+        playerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playerScript.isAlive == false)
+        {
+            CancelInvoke();
+        }
     }
       void SpawnPipes()
     {
-        Instantiate(pipesPrefab, new Vector2(10f, Random.Range(-7.1f, 3.1f)), Quaternion.identity);
+        Instantiate(pipesPrefab, new Vector2(10f, Random.Range(-5.0f, 1.5f)), Quaternion.identity);
 
     }
+
   
 }
+ 
